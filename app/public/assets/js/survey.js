972bb48f1994fd827bf9cb9c3570d4151e6de413
Vaniri <foxifennec@gmail.com>
2020-02-10 22:21:33 -0500

    added validation

--- app/public/assets/js/survey.js
+++ app/public/assets/js/survey.js
@@ -1,41 +1,63 @@
-let questionIdx = 1;
-let table = null;
-let scores = null;
+function showQuestions() {
+    let questionIdx = 1;
+    let table = null;
+    let scores = null;
+    getQuestions().forEach(question => {
+        table = $(`<li><fieldset><legend class="label">` +
+            `${question}</legend></fieldset></li>`);
+        for (let i = 1; i <= 5; i++) {
+            let id = `${questionIdx}-${i}`;
+            scores = $(`<label for="${id}" class="radio_label">` +
+                `<input type="radio" name="q${questionIdx}" id="${id}" value="${i}">` +
+                `${i} </label>`);
+            table.append(scores);
+        }
+        questionIdx++;
+        $("#question_container").append(table);
+    })
+}
 
-getQuestions().forEach(question => {
-    table = $(`<li><fieldset><legend class="label">` +
-        `${question}</legend><div id="row"></div></fieldset></li>`);
-    for (let i = 1; i <= 5; i++) {
-        let id = `${questionIdx}-${i}`;
-        scores = $(`<label for="${id}" class="radio_label">` +
-            `<input type="radio" name="q${questionIdx}" id="${id}" value="${i}">` +
-            `${i} </label>`);
-        table.append(scores);
-    }
-    questionIdx++;
-    $("#question_container").append(table);
-})
+showQuestions();
 
 $("#quiz_button").on("click", event => {
     event.preventDefault();
+    checkForm();
+})
 
-    let name = $("#name").val();
-    let photo = $("#photo").val();
+function checkForm() {
+    const fields = $(".item-required")
+        .find("form").serializeArray();
+
+    let unField = fields.filter(field => !field.value).map(field => field.name);
+
+    for (let i = 1; i <= getQuestions().length; ++i) {
+        if (!$(`input[name=q${i}]:checked`).length) {
+            unField.push("Q" + i);
+        }
+    }
+
+    if (unField.length !== 0) { alert(`Please enter ${unField.join(", ")}!`); }
+    else { getUserInfo(); }
+}
+
+function getUserInfo() {
+    const name = $("#name").val();
+    const photo = $("#photo").val();
     let scores = [];
+
     for (let i = 1; i <= getQuestions().length; ++i) {
         scores.push(+($(`input[name=q${i}]:checked`).val()));
     }
-
     let userInfo = { name, photo, scores };
 
     $.post("/api/friends", userInfo, res => {
+        console.log(res);
         $("#match_name").text(res.result.name);
         $("#match_photo").attr("src", res.result.photo);
-        console.log(res.result.name, res.result.photo)
+        console.log(res.result.name, res.result.photo);
         openModal();
     })
-
-})
+}
 
 // slideshow
 $("#slideshow > div:gt(0)").hide();
@@ -47,16 +69,12 @@ setInterval(function () {
         .fadeIn(1000)
         .end()
         .appendTo('#slideshow');
-}, 3000);
+}, 2000);
 
 
 //modal
 function openModal() {
-
     const modal = $("#myModal");
-    const btn = $("#myBtn");
-    const span = $(".close")[0];
-
     modal.css('display', 'block');
 
     $(".close").on("click", () => {
